28d007a55a42938178301d4f612719dce1b1ccfa
Douglas Duteil <douglasduteil@gmail.com>
2018-07-13 17:08:06 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+//
+
+const HYPER_COMMENT_REGEX = /_hyper: (.*?);/;
+
+const test = value => {
+  return (
+    value !== undefined &&
+    value !== null &&
+    value.nodeType === 8 &&
+    value.constructor !== undefined &&
+    value.constructor.name === 'Comment' &&
+    value.textContent !== undefined &&
+    HYPER_COMMENT_REGEX.test(value.textContent)
+  );
+};
+
+const print = () => '';
+
+module.exports = {
+  print: print,
+  test: test
+};
